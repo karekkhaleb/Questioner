3321d496a9bfef367f06cc97449896b1706123b3
Buhungiro Caleb <karekkhaleb@gmail.com>
2018-12-24 17:06:14 +0200

    [Feature #162831461] add the get all meetups api endpoint

--- src/routes/meetupRoutes.js
+++ src/routes/meetupRoutes.js
@@ -3,6 +3,7 @@ import meetupController from '../controllers/meetupController';
 
 const Router = express.Router();
 
+Router.get('/', meetupController.getAll);
 Router.post('/', meetupController.create);
 
 

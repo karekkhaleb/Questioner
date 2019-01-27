6d52299dab2cbaeb893b9240d43b308cd18ea7b3
karekkhaleb <karekkhaleb@github.com>
2019-01-27 18:42:59 +0200

    drop tables after tests: use the before all function

--- server/tests/server/baseUrl.spec.js
+++ server/tests/server/baseUrl.spec.js
@@ -1,6 +1,23 @@
 import request from 'request';
 import '../../app';
 import { baseUrl } from './testUtils';
+import { executeQuery } from '../../db';
+
+afterAll(async (DONE) => {
+  const query = `
+      drop table votes;
+      drop table images;
+      drop table meetups_tags;
+      drop table tags;
+      drop table comments;
+      drop table questions;
+      drop table rsvps;
+      drop table meetups;
+      drop table users;
+  `;
+  await executeQuery(query);
+  DONE();
+});
 
 describe('Testing the base url', () => {
   it('should give a proper api endpoint', (done) => {
